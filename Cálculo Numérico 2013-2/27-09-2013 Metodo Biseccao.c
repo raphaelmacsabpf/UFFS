16393f619bc5a50909d7d19682b29d29eeaf662d
raphaelmacsabpf <raphaelmacsabpf@gmail.com>
2013-10-31 17:52:16 -0200

    Iniciando Trabalho de jogos

--- Cálculo Numérico 2013-2/27-09-2013 Metodo Biseccao.c
+++ Cálculo Numérico 2013-2/27-09-2013 Metodo Biseccao.c
@@ -1,14 +1,60 @@
 #include <stdio.h>
 #include <math.h>
+
 float absoluto(float x);
 float funcao(float x);
+float derivada(float x);
+
 float e=2.7182;
+float a = 0;
+float b = 1;
+float eps = pow(10,-7);
 int main()
 {
-    float a = -100;
-    float b = 49;
+    biseccao();
+    //cordas();
+    //newton();
+    return 1;
+}
+void cordas()
+{
     float fa,fb;
-    float eps = 0.001;
+    float r, fr;
+    int i = 0;
+    fr = (b-a)/2;
+    for(i = 0; ; i++)
+    {
+        printf("CORDAS ITEROU\n\n");
+        fr = ((a*funcao(b))-(b*funcao(a)))/(funcao(b) - funcao(a));
+        if(funcao(fr)*funcao(b) < 0)
+            a = fr;
+        else
+            b = fr;
+        i++;
+        if(absoluto(funcao(fr)) < eps)
+            break;
+
+    }
+    printf("CORDAS RESULTOU EM RAIZ = %f com %d Iteradas\n",fr,i);
+}
+void newton()
+{
+    float fr;
+    float na = a;
+    int i = 0;
+    do {
+
+        fr = na;
+        na = na - funcao(na)/derivada(na);
+        float err = absoluto((na - fr) / na);
+        i++;
+
+    }while((float)na+eps <fr);
+    printf("NEWTON RESULTOU EM RAIZ = %f com %d Iteradas\n",na,i);
+}
+void biseccao()
+{
+    float fa,fb;
     float r, fr;
     int i = 0;
     r = (a+b)/2;
@@ -24,14 +70,24 @@ int main()
         fr = funcao(r);
         i++;
     }
-    printf("R = %f",r);
-    return 1;
+    printf("BISECCAO RESULTOU EM RAIZ = %f com %d Iteradas\n",r,i);
 }
-
 float funcao(float x)
 {
-    return x*x;
+    return ((1-(pow((1+x),-12)))/x)-((312000-91051.90)/26000);
 }
+float derivada(float x)
+{
+    return 3* pow(x,2);
+}
+/*float funcao_der1(float x)
+{pow((1+x),-12)-((312000-91051.90)/26000)
+    return (e^x)*(-1);
+}
+float funcao_der2(float x)
+{
+    return (e^x)*(-1);
+}*/
 float absoluto(float x)
 {
     if(x < 0)
